39386dc37fd2ea7e3f48992842dea654f9cfe999
Parth Patel <parth_patel@comcast.com>
2020-05-06 17:59:10 -0400

    load data with insertMany of mongodb

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const MongoClient = require('mongodb').MongoClient;
 
+const CirculationRepo = require('./repos/circulationRepo');
+const data = require('./circulation.json');
+
 const url = 'mongodb://localhost:27017';
 const dbName = 'circulation';
 
 async function main() {
     const client = new MongoClient(url);
     await client.connect();
+
+    const results = await CirculationRepo.loadData(data);
+    console.log(results.insertedCount, results.ops);
 
     const admin = client.db(dbName).admin();
 
